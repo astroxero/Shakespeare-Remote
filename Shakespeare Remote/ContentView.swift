3d355e5c102cc9dc6fb32c9ee8484899e234d884
Beau Rubin-Rottenberg <beaurubrot@gmail.com>
2023-11-13 19:33:46 -0500

    Initial Commit

---
+++ Shakespeare Remote/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Shakespeare Remote
+//
+//  Created by Beau Rubin-Rottenberg on 11/13/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Shakespeare Remote/Shakespeare_RemoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shakespeare_RemoteApp.swift
+//  Shakespeare Remote
+//
+//  Created by Beau Rubin-Rottenberg on 11/13/23.
+//
+
+import SwiftUI
+
+@main
+struct Shakespeare_RemoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
