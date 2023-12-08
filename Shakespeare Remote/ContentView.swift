8cb986042aa62c76d7bed3480b4c13e4d2544f8c
Beau Rubin-Rottenberg <beaurubrot@gmail.com>
2023-12-08 17:06:16 -0500

    scalebutton

--- Shakespeare Remote/ContentView.swift
+++ Shakespeare Remote/ContentView.swift
@@ -9,43 +9,52 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        @StateObject var viewModel = ContentViewModel()
+        @StateObject var viewModel: ContentViewModel = ContentViewModel()
         @State var scale = 1.0
-
+        
         VStack {
             HStack {
                 TLButton(title: "Daytime", bgColor: .yellow) {
                     viewModel.sendMessage(message: "daytime")
                 }
+                .buttonStyle(ScaleButton())
                 
                 TLButton(title: "Pre-Show", bgColor: .orange) {
                     viewModel.sendMessage(message: "evening")
                 }
+                .buttonStyle(ScaleButton())
             }
             HStack {
                 TLButton(title: "Party", bgColor: .pink) {
                     viewModel.sendMessage(message: "party")
                 }
+                .buttonStyle(ScaleButton())
                 TLButton(title: "Idle", bgColor: .blue) {
                     viewModel.sendMessage(message: "after")
                 }
+                .buttonStyle(ScaleButton())
             }
             HStack {
                 TLButton(title: "Next", bgColor: .green) {
                     viewModel.sendMessage(message: "next")
                 }
+                .buttonStyle(ScaleButton())
                 TLButton(title: "Photo", bgColor: .purple) {
                     viewModel.sendMessage(message: "photo")
                     
                 }
+                .buttonStyle(ScaleButton())
             }
             HStack {
                 TLButton(title: "Test", bgColor: .black) {
                     viewModel.sendMessage(message: "test")
                 }
+                .buttonStyle(ScaleButton())
+                .zIndex(0)
                 TLButton(title: "OFF", bgColor: .red) {
                     viewModel.sendMessage(message: "black")
                 }
+                .buttonStyle(ScaleButton())
             }
         }
     }
--- Shakespeare Remote/ScaleButton.swift
+++ Shakespeare Remote/ScaleButton.swift
@@ -5,4 +5,12 @@
 //  Created by Beau Rubin-Rottenberg on 12/8/23.
 //
 
-import Foundation
+import SwiftUI
+
+struct ScaleButton : ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .scaleEffect(configuration.isPressed ? 1.5 : 1)
+                    .animation(.spring(response: 0.4, dampingFraction: 0.5, blendDuration: 0), value: configuration.isPressed)
+    }
+}
