ff2f04e2b9aee494c5aa7a0a5db751a446676a2d
Beau Rubin-Rottenberg <beaurubrot@gmail.com>
2023-12-09 16:25:21 -0500

    haptics

---
+++ Shakespeare Remote/AppSettings.swift
@@ -0,0 +1,8 @@
+//
+//  AppSettings.swift
+//  Shakespeare Remote
+//
+//  Created by Beau Rubin-Rottenberg on 12/9/23.
+//
+
+import Foundation
---
+++ Shakespeare Remote/Haptics.swift
@@ -0,0 +1,8 @@
+//
+//  Haptics.swift
+//  Shakespeare Remote
+//
+//  Created by Beau Rubin-Rottenberg on 12/9/23.
+//
+
+import Foundation
